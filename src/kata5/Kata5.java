0e78f6c30bd8cddc1aa662497ebeeeb44ea23bed
J0RG3PD99 <J0RG3PD99@MSI>
2019-12-03 16:37:10 +0000

    Kata5 Finished

---
+++ src/kata5/Kata5.java
@@ -0,0 +1,23 @@
+package kata5;
+
+public class Kata5 {
+
+    
+    public static void main(String[] args) {
+        String url = "jdbc:sqlite:C:\\Users\\Saulo\\Documents\\NetBeansProjects\\kata5\\sqllite\\miercoles.db.db";
+        DataBase database = new DataBase(url);
+        database.open();
+
+        database.select_PERSONAS();
+        
+        People people = new People("Santiago","Sanchez","Marketing");
+        database.insert_PERSONAS(people);
+ 
+        System.out.println(" * * * * * * * * *");
+        
+        database.select_PERSONAS();
+        
+        database.close();
+    }
+    
+}
---
+++ src/kata5/People.java
@@ -0,0 +1,24 @@
+package kata5;
+
+public class People {
+    String Nombre;
+    String Apellidos;
+    String departamento;
+    public People(String nombre, String apellidos, String d) {
+        this.Nombre = nombre;
+        this.Apellidos = apellidos;
+        this.departamento = d;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+}
